876cc9b3825b87866204c1dec5329919d1351057
Pho3nixG0D <oliverkrska@gmail.com>
2023-01-19 17:16:16 +0100

    Created second window

---
+++ HousingEstate02/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HousingEstate02.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingEstate02.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+    }
+}
